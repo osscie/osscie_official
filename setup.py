d8a6e49439acb87f142a1b28f72b0432779ee3ca
Osscie <34659748+osscie@users.noreply.github.com>
2017-12-18 17:40:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
-# setup.py
+from setuptools import setup, find_packages
+
+cfg = dict(
+    name             = 'osscie_official',
+    version          = '1.0.0',
+    description      = (
+        'Official django package of Osscie.com'
+    ),
+    packages         = find_packages(),
+    zip_safe         = False,
+    include_package_data = True,
+    install_requires = [
+        'requests'
+    ]
+)
+
+if __name__ == '__main__':
+    setup(**cfg)
